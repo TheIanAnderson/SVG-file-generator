3d8e42440031ecbafc4ea12fd77c5ef81b8f23ef
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-07-27 16:14:02 -0500

    Add validate in inquirer prompt, emplimented working logic for it.

---
+++ develop/index.js
@@ -0,0 +1,43 @@
+const inquirer = require("inquirer");
+const colorValidation = [];
+
+const countLetters = (input) => {
+  const stringLength = input.length;
+  if (stringLength != 3) {
+    return "Length must be between 1 and 3 letters.";
+  }
+  return true;
+};
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "What text would you like on the logo?",
+      validate: countLetters,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What color would you like the text to be?",
+      // validation: "#"
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "What shape would you like it to be?",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "What color would you like the shape to be?",
+      // validation: "#"
+    },
+  ])
+  .then((data) => {
+    if ((data = true)) {
+      console.log("thank you for your answers!");
+    }
+  });
